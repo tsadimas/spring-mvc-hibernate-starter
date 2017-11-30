a0c1d2c35bc49dc967c2031ed229f6b652214aeb
Anargyros Tsadimas <tsadimas@gmail.com>
2017-11-30 14:10:07 +0200

    first commit

---
+++ src/gr/hua/dit/controller/CustomerController.java
@@ -0,0 +1,33 @@
+package gr.hua.dit.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import gr.hua.dit.dao.CustomerDAO;
+import gr.hua.dit.entity.Customer;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	// inject the customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@RequestMapping("/list")
+	public String listCustomers(Model model) {
+		
+		// get customers from dao
+		List<Customer> customers = customerDAO.getCustomers();
+		
+		// add the customers to the model
+		model.addAttribute("customers",customers);
+		
+		return "list-customers";
+	}
+
+}
---
+++ src/gr/hua/dit/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package gr.hua.dit.dao;
+
+import java.util.List;
+
+import gr.hua.dit.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+	
+}
