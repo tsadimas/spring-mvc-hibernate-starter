c8c63094c618270f1793b4438a65e8c3dbe7545d
tsadimas <tsadimas@hua.gr>
2018-01-17 17:17:36 +0200

    show username and roles in footer

--- src/gr/hua/dit/api/StudentApiController.java
+++ src/gr/hua/dit/api/StudentApiController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,7 @@ import gr.hua.dit.service.StudentService;
 
 @RestController
 @RequestMapping("/api/student")
+@CrossOrigin(origins = "*", maxAge=3600)
 public class StudentApiController {
 
 	@Autowired
